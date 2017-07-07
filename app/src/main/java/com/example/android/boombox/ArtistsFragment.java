aab4e243c4a0149b5a7afa0b4e191a30d3df080b
Cordlesswire <chungwoo007@gmail.com>
2017-07-07 11:34:44 +0200

    Initial Commit

--- app/src/main/java/com/example/android/boombox/ArtistsFragment.java
+++ app/src/main/java/com/example/android/boombox/ArtistsFragment.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SeekBar;
 
 import java.util.ArrayList;
 
@@ -18,6 +21,24 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class ArtistsFragment extends Fragment {
+    //Declare Variables
+    private Handler songHandler = new Handler();
+
+    private ImageView previousButton, rewindButton, pauseButton, playButton, forwardButton, nextButton;
+    private SeekBar seekBar;
+    //private TextView startTimeView, songTitle, endTimeView;
+
+    private double startTime = 0;
+    private double finalTime = 0;
+
+    private int forwardTime = 5000;
+    private int backwardTime = 5000;
+
+    //Keeps track of Current Song
+    private int currentIndex = 0;
+
+
+
     private MediaPlayer mMediaPlayer ;
     private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener(){
         @Override
@@ -48,12 +69,29 @@ public class ArtistsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public static int oneTimeOnly = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.words_list,container,false);
+
+        //Initialize Views and Buttons
+        previousButton = (ImageView) rootView.findViewById(R.id.previousButton);
+        rewindButton = (ImageView) rootView.findViewById(R.id.rewindButton);
+        pauseButton = (ImageView) rootView.findViewById(R.id.pauseButton);
+        playButton = (ImageView) rootView.findViewById(R.id.playButton);
+        forwardButton = (ImageView) rootView.findViewById(R.id.forwardButton);
+        nextButton = (ImageView) rootView.findViewById(R.id.nextButton);
+
+        seekBar = (SeekBar) rootView.findViewById(R.id.seekBar);
+        seekBar.setClickable(false);
+
+        //startTimeView = (TextView) rootView.findViewById(R.id.startTime);
+        //songTitle = (TextView) rootView.findViewById(R.id.songTitle);
+        // endTimeView = (TextView) rootView.findViewById(R.id.endTime);
+
 
         mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
 
@@ -70,27 +108,162 @@ public class ArtistsFragment extends Fragment {
         songs.add(new Word("Tupac Shakur","number of songs in memory",R.drawable.tupac,R.raw.gas_lab_jazz_hop));
         songs.add(new Word("Willow","number of songs in memory",R.drawable.willow,R.raw.willow_9));
 
-
-        WordAdapter itemsAdapter = new WordAdapter(getActivity(),songs,R.color.category_colors);
+        WordAdapter itemsAdapter = new WordAdapter(getActivity(), songs, R.color.category_colors);
         ListView listView = (ListView) rootView.findViewById(R.id.list);
+        //final LinearLayout musicControls = (LinearLayout) rootView.findViewById(R.id.musicControls);
+
 
         listView.setAdapter(itemsAdapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                //Hide the music controls until a user clicks on a song for better User Experience
+                // musicControls.setVisibility(View.VISIBLE);
                 Word word = songs.get(position);
                 releaseMediaPlayer();
+
                 int requestResult = mAudioManager.requestAudioFocus(afListener,
                         AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-                if(requestResult==AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
-                {
-                    mMediaPlayer = MediaPlayer.create(getActivity(),word.getSoundResourceId());
+                if (requestResult == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer = MediaPlayer.create(getActivity(), word.getSoundResourceId());
                     mMediaPlayer.start();
                     mMediaPlayer.setOnCompletionListener(mCompletionListener);
+
+
+
+                    finalTime = mMediaPlayer.getDuration();
+                    startTime = mMediaPlayer.getCurrentPosition();
+
+                    if (oneTimeOnly == 0) {
+                        seekBar.setMax((int) finalTime);
+                        oneTimeOnly = 1;
+                    }
+
+
+
+
+                    //Methods to Display Song Duration
+                    //endTimeView.setText(String.format("%d min, %d sec",
+                    //TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                    //TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                    // TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                    //  finalTime)))
+                    //);
+
+                    //startTimeView.setText(String.format("%d min, %d sec",
+                    //TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    //TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                    //TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                    //startTime)))
+                    // );
+
+
+                    seekBar.setProgress((int) startTime);
+                    seekBar.setMax(mMediaPlayer.getDuration());
+                    songHandler.postDelayed(UpdateSongTime, 100);
+
+
+                }
+
+            }
+        });
+
+
+        //Method to fast forward the track
+        forwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int) startTime;
+                if ((temp+forwardTime)<=finalTime) {
+                    startTime = startTime + forwardTime;
+                    mMediaPlayer.seekTo((int) startTime);
                 }
             }
         });
+
+
+        //Method to rewind the track
+        rewindButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int) startTime;
+                if ((temp-backwardTime)> 0) {
+                    startTime = startTime - backwardTime;
+                    mMediaPlayer.seekTo((int) startTime);
+                }
+            }
+        });
+
+
+
+
+        //Method to Play Previous Song
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer.isPlaying()) {
+                    seekBar.setProgress(0);
+
+                }
+            }
+        });
+
+
+
+
+
+        //Method to Play Next Song
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //int temp = (int) finalTime;
+                if (mMediaPlayer.isPlaying()) {
+                    seekBar.setProgress(0);
+
+                    // mMediaPlayer.setDataSource();
+                    // mMediaPlayer.start();
+                }
+            }
+        });
+
+
+
+
+        //Method to Pause song
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
+                    //"Pause" is not in display so the user can see the "Play" Button to Resume music
+                    pauseButton.setVisibility(View.GONE);
+                    //Displays "Play" button and replaces the "Pause" button on the controls layout
+                    playButton.setVisibility(View.VISIBLE);
+
+                    previousButton.setEnabled(false);
+                    forwardButton.setEnabled(false);
+                    nextButton.setEnabled(false);
+                    rewindButton.setEnabled(false);
+                }
+            }
+        });
+
+
+        //Method to Play song if it was Paused
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMediaPlayer.start();
+                //"Play" is not in display so the user can see the "Pause" Button to Pause the music if needed
+                playButton.setVisibility(View.GONE);
+                //Displays "Pause" button and replaces the "Play" button on the controls layout
+                pauseButton.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
 
         return rootView;
     }
@@ -113,8 +286,21 @@ public class ArtistsFragment extends Fragment {
             // is not configured to play an audio file at the moment.
             mMediaPlayer = null;
             mAudioManager.abandonAudioFocus(afListener);
-
         }
     }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            startTime = mMediaPlayer.getCurrentPosition();
+            //startTimeView.setText(String.format("%d min, %d sec",
+            //       TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+            //       TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+            //              TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+            //                       toMinutes((long) startTime)))
+            // );
+            seekBar.setProgress((int) startTime);
+            songHandler.postDelayed(this, 100);
+        }
+    };
 
 }
